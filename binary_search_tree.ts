914e6d906b729e25c77b18e375a3fb3d5e3a21cf
locoer <locoer@gmail.com>
2022-08-10 10:45:29 -0500

    Added insert value method to BST

--- binary_search_tree.ts
+++ binary_search_tree.ts
@@ -4,7 +4,7 @@
 */
 
 class BinaryST {
-  nodes : number[];
+  nodes : TreeNode[];
   rootNode : TreeNode;
   treeHeight : number;
 
@@ -12,7 +12,36 @@ class BinaryST {
     this.treeHeight = 0
   }
 
-  insertValue() {
+  insertValue( val : number, node : TreeNode | null = this.rootNode ) : void {
+    if( node === null ) return
+    
+    if( this.nodes.length === 0 ) {
+      let newNode : TreeNode = new TreeNode(val, 0)
+      this.rootNode = newNode
+      this.nodes.push(newNode)
+      return
+    }
+
+    //Checks direction left (less than node)
+    if( val <= node.value ) {
+      if( node.leftChild === null ) {
+        let newNode : TreeNode = new TreeNode(val, node.height + 1)
+        node.leftChild = newNode
+        this.nodes.push(newNode)
+        return
+      } else {
+        this.insertValue(val, node.leftChild)
+      }
+    } else { //Direction right (bigger than node)
+        if( node.rightChild === null ) {
+          let newNode : TreeNode = new TreeNode(val, node.height + 1)
+          node.rightChild = newNode
+          this.nodes.push(newNode)
+          return
+        } else {
+          this.insertValue(val, node.rightChild)
+        }
+    }
 
   }
 
