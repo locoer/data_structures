324ba0c4df931977a2fd37850c79308e4adf76bf
locoer <locoer@gmail.com>
2022-08-11 11:09:19 -0500

    Added 2 of 3 cases for remove method

--- binary_search_tree.ts
+++ binary_search_tree.ts
@@ -18,7 +18,7 @@ class BinaryST {
   insertValue( val : number, node : TreeNode | null = this.rootNode ) : void {
     if( node === null ) return
     
-    if( this.nodes.length === 0 ) {
+    if( this.nodes.length === 0 ) { //Inserts Root Node
       let newNode : TreeNode = new TreeNode(val, 0)
       this.rootNode = newNode
       this.nodes.push(newNode)
@@ -31,6 +31,7 @@ class BinaryST {
       if( node.leftChild === null ) {
         let newNode : TreeNode = new TreeNode(val, node.height + 1)
         node.leftChild = newNode
+        newNode.parent = node
         this.nodes.push(newNode)
         if( newNode.height > this.treeHeight ) this.treeHeight = newNode.height
         return
@@ -41,6 +42,7 @@ class BinaryST {
         if( node.rightChild === null ) {
           let newNode : TreeNode = new TreeNode(val, node.height + 1)
           node.rightChild = newNode
+          newNode.parent = node
           this.nodes.push(newNode)
           if( newNode.height > this.treeHeight ) this.treeHeight = newNode.height
           return
@@ -51,14 +53,50 @@ class BinaryST {
 
   }
 
-  removeValue(){
+  removeValue( value : number ){
+    //search value, if is not in the tree do nothing
+    let removeNode = this.search(value)
+
+    if( removeNode === false) return
+
+    //case 1 - TreeNode is a leaf (no children), then delete it
+    if( removeNode.leftChild === null && removeNode.rightChild === null ) {
+      let parent : TreeNode | null = removeNode.parent
+      if( parent !== null ) {
+        parent.leftChild?.value === removeNode.value ? parent.leftChild = null : parent.rightChild = null
+        return
+      }
+    }
+    let parent = removeNode.parent
+    //case 2 - TreeNode only has one child, then swap places with child and delete node.
+    if ( removeNode.leftChild !== null && removeNode.rightChild === null ) {
+      if( parent !== null ) {
+        parent?.leftChild?.value === removeNode.value ? parent.leftChild = removeNode.leftChild : parent.rightChild = removeNode.leftChild
+      }
+    }
+    if ( removeNode.rightChild !== null && removeNode.leftChild === null ) {
+      if( parent !== null ) {
+        parent?.leftChild?.value === removeNode.value ? parent.leftChild = removeNode.rightChild : parent.rightChild = removeNode.rightChild
+        
+      }
+    }
+
+    //case 3 - TreeNode has two children, then search for "in order succesor", swap places and delet node.
+    // "in order succesor" the next leaf node that is bigger than leftChild and smaller than rightChild
+
+    //Search for available leaf values that can be swapped
+
+    //Sets to null the Node Values
+    removeNode.leftChild = null
+    removeNode.rightChild = null
+    removeNode.parent = null
 
   }
 
-  search( value : number, node : TreeNode = this.rootNode) : boolean {
-    let foundValue : boolean = false
+  search( value : number, node : TreeNode = this.rootNode) : TreeNode | false {
+    let foundValue : TreeNode | boolean = false
     if( value === node.value ) {
-      foundValue = true
+      foundValue = node
     } else {
       if( value <= node.value && node.leftChild !== null ) return this.search(value, node.leftChild)
       if( value > node.value && node.rightChild !== null ) return this.search(value, node.rightChild)
@@ -74,11 +112,13 @@ class TreeNode {
   height : number;
   leftChild : TreeNode | null;
   rightChild : TreeNode | null;
+  parent : TreeNode | null;
 
   constructor(val:number, height: number){
     this.value = val
     this.height = height
     this.leftChild = null
     this.rightChild = null
+    this.parent = null
   }
 }
